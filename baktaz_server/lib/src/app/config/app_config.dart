b0326a725f6bd5accd8d3339a59688f2401dd74e
Arnoodles <aj.lum07@gmail.com>
2026-08-21 01:35:12 +0800

    feat(server): add OtpConfig constants

--- baktaz_server/lib/src/app/config/app_config.dart
+++ baktaz_server/lib/src/app/config/app_config.dart
@@ -2,3 +2,12 @@ class AppConfig {
   static const Duration defaultTimeout = Duration(minutes: 1);
   static const Duration defaultCacheLifetime = Duration(minutes: 5);
 }
+
+class OtpConfig {
+  static const int length = 6;
+  static const Duration expiry = Duration(minutes: 5);
+  static const int maxSendsPerHour = 3;
+  static const int maxAttemptsPerOtp = 3;
+  static const Duration registrationTokenLifetime = Duration(minutes: 15);
+  static const String method = 'email_otp';
+}
